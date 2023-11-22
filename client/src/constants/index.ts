3b389bd53c3519f3de575566afca6ffe94455b2e
Son Do <dominhson580@gmail.com>
2023-11-22 17:08:41 +0700

    Define constants

---
+++ client/src/constants/index.ts
@@ -0,0 +1,13 @@
+// Color
+const WHITE = "w";
+const BLACK = "b";
+
+// Piece
+const PAWN = "p";
+const KNIGHT = "n";
+const BISHOP = "b";
+const ROOK = "r";
+const QUEEN = "q";
+const KING = "k";
+
+export { WHITE, BLACK, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING };
