92b97aa3adf306a991010ff0e162fd42cce52317
Son Do <dominhson580@gmail.com>
2023-11-26 15:12:49 +0700

    Check for pawn attacks

--- client/src/attacks/index.ts
+++ client/src/attacks/index.ts
@@ -4,6 +4,7 @@ import { getOpponentColor, getPiece, posParse } from "../utils";
 import { getKingPos } from "../utils/king";
 
 import { getKnightAttacks, isAttackedByKnight } from "./knight";
+import { getPawnAttacks, isAttackedByPawn } from "./pawn";
 
 // Get the king that is being attacked
 const getAttackedKing = (board: Board, turn: Color): King | null => {
@@ -12,7 +13,9 @@ const getAttackedKing = (board: Board, turn: Color): King | null => {
   const [row, col] = posParse(kingPos);
   const opponentColor = getOpponentColor(turn);
 
-  const isAttacked = isAttackedByKnight(board, row, col, opponentColor);
+  const isAttacked =
+    isAttackedByKnight(board, row, col, opponentColor) ||
+    isAttackedByPawn(board, row, col, opponentColor);
 
   return isAttacked ? currentKing : null;
 };
@@ -23,7 +26,10 @@ const getAttacks = (board: Board, turn: Color): SquarePos[] => {
   const [row, col] = posParse(kingPos);
   const opponentColor = getOpponentColor(turn);
 
-  return [...getKnightAttacks(board, row, col, opponentColor)];
+  return [
+    ...getKnightAttacks(board, row, col, opponentColor),
+    ...getPawnAttacks(board, row, col, opponentColor),
+  ];
 };
 
 export { getAttackedKing, getAttacks };
