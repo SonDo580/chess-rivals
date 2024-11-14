3de56738f33b754368ef23f0c40823f88557af6a
Son Do <dominhson580@gmail.com>
2024-11-14 08:29:35 +0700

    use class

--- server/src/controllers/game.ts
+++ server/src/controllers/game.ts
@@ -184,199 +184,241 @@ const promotionHandler =
 
 // ===== REFACTOR selectSquareHandler =====
 
-/* First click: highlight the valid moves
-   Second click: make a valid move, or just clear the highlighting */
-const selectSquareHandler2 =
-  (socket: Socket, io: Server) =>
-  (roomId: string, row: number, col: number) => {
-    // Find the room
-    const room = searchRoomById(roomId);
+// File: notifier.ts
 
-    // Check if the game has already ended
-    if (isGameEnded(room)) {
-      return;
-    }
-
-    const position = posString(row, col); // string format: row-col
-
-    // (second click) there is a current selection and the move is valid
-    if (room.currentSquare && room.moves.includes(position)) {
-      // Make a move & update room state
-      processMove(room, position);
-
-      notifyPlayers(socket, io, room);
-      return;
-    }
-
-    // (first click) Handle selecting a new piece
-
-    if (!isSquareValidForSelection(room, row, col)) {
-      clearSelection(room); // clear highlighting
-      notifyPlayers(socket, io, room);
-      return;
-    }
-
-    // Set selected square
-    room.currentSquare = position;
-
-    // Update valid moves
-    room.moves = getValidMoves(room, row, col);
-
-    // Update highlighting
-    room.squaresToHighlight = [position, ...room.moves];
-
-    notifyPlayers(socket, io, room);
-  };
-
-enum EventName {
+export enum EventName {
   ROOM_UPDATED = "roomUpdated",
-  // ...
+  SELECT_SQUARE = "selectSquare",
+  // ... other events
 }
 
-// Notify players in the room
-function notifyPlayers(socket: Socket, io: Server, room: Room) {
-  // notify current player
-  socket.emit(EventName.ROOM_UPDATED, room);
+interface NotifyCurrentPlayerParams<T> {
+  eventName: EventName;
+  data?: T;
+}
 
-  // notify the other player if they joined
-  const { otherPlayer } = getPlayers(room, socket.id);
-  if (otherPlayer) {
-    io.to(otherPlayer.id).emit(EventName.ROOM_UPDATED, room);
+interface NotifyBothPlayersParams<T> extends NotifyCurrentPlayerParams<T> {
+  room: Room;
+}
+
+export class Notifier {
+  constructor(private readonly socket: Socket, private readonly io: Server) {}
+
+  /* Notify players in the room */
+  public notifyPlayers<T>({
+    eventName,
+    data,
+    room,
+  }: NotifyBothPlayersParams<T>) {
+    this.notifyCurrentPlayer({ eventName, data });
+    this.notifyOtherPlayer({ eventName, data, room });
+  }
+
+  /* Notify current player */
+  public notifyCurrentPlayer<T>({
+    eventName,
+    data,
+  }: NotifyCurrentPlayerParams<T>) {
+    this.socket.emit(eventName, data);
+  }
+
+  /* Notify the other player if they joined */
+  private notifyOtherPlayer<T>({
+    eventName,
+    data,
+    room,
+  }: NotifyBothPlayersParams<T>) {
+    const currentPlayerId = this.socket.id;
+    const { otherPlayer } = getPlayers(room, currentPlayerId);
+
+    if (otherPlayer) {
+      this.io.to(otherPlayer.id).emit(eventName, data);
+    }
   }
 }
 
-// Check if the game is already ended
-function isGameEnded(room: Room) {
-  return room.result.kind !== undefined;
-}
+// ==============================
 
-// Make a move & update room state
-function processMove(room: Room, position: SquarePos) {
-  // Extract the room information
-  const { turn, board, enPassant, castlingRights } = room;
-  const currentSquare = room.currentSquare as SquarePos;
+// File: game.controller.ts
+export class GameController {
+  /**
+   * Handle SELECT_SQUARE event
+   * - first click: highlight the valid moves
+   * - second click: make a valid move or clear highlight
+   */
+  public static selectSquareHandler(socket: Socket, io: Server) {
+    const notifier = new Notifier(socket, io);
 
-  // Make the move -> update board state
-  room.board = makeMove(
-    board,
-    currentSquare,
-    position,
-    enPassant,
-    castlingRights[turn]
-  );
+    return (roomId: string, row: number, col: number) => {
+      const room = searchRoomById(roomId);
 
-  // Handle highlighting state
-  room.lastMove = position;
-  clearSelection(room);
+      // Check if the game has already ended
+      if (GameController.isGameEnded(room)) {
+        return;
+      }
 
-  // The current king should not be in danger now. Reset 'check' state
-  // -> the function name is not good
-  checkAttacks(room);
+      const position = posString(row, col);
 
-  // Update castling right for current player
-  updateCastlingRights(room, currentSquare);
+      // Make a move if there's a current selection and the move is valid
+      if (room.currentSquare && room.moves.includes(position)) {
+        GameController.processMove(room, position);
+        GameController.notifyPlayers(notifier, room);
+        return;
+      }
 
-  // Update en passant state
-  updateEnPassant(room, currentSquare, position);
+      // Handle selecting a new piece
+      if (!GameController.isSquareValidForSelection(room, row, col)) {
+        clearSelection(room);
+        GameController.notifyPlayers(notifier, room);
+        return;
+      }
 
-  // Check pawn promotion availability & update state.
-  // Return early (don't swap turn)
-  if (needPromotion(room.board, position, turn)) {
-    room.needPromotion = true;
-    return;
+      // Update selected square, valid moves, highlighting
+      room.currentSquare = position;
+      room.moves = GameController.getValidMoves(room, row, col);
+      room.squaresToHighlight = [position, ...room.moves];
+
+      GameController.notifyPlayers(notifier, room);
+    };
   }
 
-  // Update state for 50-move rule
-  update50MoveState(room, currentSquare, position);
-
-  // Swap turn
-  swapTurn(room);
-
-  // check if the opponent king is under attack
-  // & update the room's attacked king -> the name 'check' is not good
-  checkAttacks(room);
-
-  // Check for end game
-  // & update result if needed -> the name 'check' is not good
-  checkEndGame(room);
-}
-
-// Update castling right for current player
-function updateCastlingRights(room: Room, currentSquare: SquarePos) {
-  const { turn, board, castlingRights } = room;
-  const castlingRight = castlingRights[turn];
-
-  // Skip if castling right has been removed for both side (king & queen)
-  if (!castlingRight.q && !castlingRight.k) {
-    return;
+  private static notifyPlayers(notifier: Notifier, room: Room) {
+    notifier.notifyPlayers({
+      eventName: EventName.ROOM_UPDATED,
+      room,
+      data: room,
+    });
   }
 
-  room.castlingRights[turn] = updateCastlingRight(
-    board,
-    currentSquare,
-    castlingRight
-  );
-}
-
-// Update en passant state
-function updateEnPassant(
-  room: Room,
-  currentSquare: SquarePos,
-  position: SquarePos
-) {
-  // Reset after making a valid move
-  if (room.enPassant.pieces.length > 0) {
-    room.enPassant = defaultEnPassantInfo;
-    return;
+  /* Check if the game is already ended */
+  private static isGameEnded(room: Room) {
+    return room.result.kind !== undefined;
   }
 
-  // Check & update en passant state
-  // -> the function name is not good
-  room.enPassant = checkEnPassant(
-    room.board,
-    currentSquare,
-    position,
-    room.turn
-  );
-}
+  /* Make a move & update room state */
+  private static processMove(room: Room, position: SquarePos) {
+    const { turn, board, enPassant, castlingRights } = room;
+    const currentSquare = room.currentSquare as SquarePos;
 
-// Update state for 50-Move rule
-function update50MoveState(
-  room: Room,
-  currentSquare: SquarePos,
-  position: SquarePos
-) {
-  // Reset if there are pawn moves and capturing
-  if (shouldReset50Move(room.board, currentSquare, position)) {
-    room.fiftyMoveCount = 0;
-    return;
+    // Update board state after making thee move
+    room.board = makeMove(
+      board,
+      currentSquare,
+      position,
+      enPassant,
+      castlingRights[turn]
+    );
+
+    // Handle highlighting state
+    room.lastMove = position;
+    clearSelection(room);
+
+    // The current king should not be in danger now. Reset 'check' state
+    // -> the function name is not good
+    checkAttacks(room);
+
+    GameController.updateCastlingRights(room, currentSquare);
+    GameController.updateEnPassant(room, currentSquare, position);
+
+    // Handle pawn promotion
+    if (needPromotion(room.board, position, turn)) {
+      room.needPromotion = true;
+      return;
+    }
+
+    GameController.update50MoveState(room, currentSquare, position);
+
+    swapTurn(room);
+
+    // check if the opponent king is under attack & update the room's attacked king 
+    // -> the name 'check' is not good
+    checkAttacks(room);
+
+    // Check for end game & update result if needed 
+    // -> the name 'check' is not good
+    checkEndGame(room);
   }
-  room.fiftyMoveCount++;
-}
 
-// Check if selected square is valid (first click)
-function isSquareValidForSelection(room: Room, row: number, col: number) {
-  const square = room.board[row][col];
+  /* Update castling right for current player */
+  private static updateCastlingRights(room: Room, currentSquare: SquarePos) {
+    const { turn, board, castlingRights } = room;
+    const castlingRight = castlingRights[turn];
 
-  // Select an empty square
-  if (!square) {
-    return false;
+    // Skip if castling right has been removed for both side (king & queen)
+    if (!castlingRight.q && !castlingRight.k) {
+      return;
+    }
+
+    room.castlingRights[turn] = updateCastlingRight(
+      board,
+      currentSquare,
+      castlingRight
+    );
   }
 
-  // Select the same piece twice
-  if (room.currentSquare === posString(row, col)) {
-    return false;
+  /* Update en-passant state */
+  private static updateEnPassant(
+    room: Room,
+    currentSquare: SquarePos,
+    position: SquarePos
+  ) {
+    // Reset after making a valid move
+    if (room.enPassant.pieces.length > 0) {
+      room.enPassant = defaultEnPassantInfo;
+      return;
+    }
+
+    // Check & update en-passant state
+    // -> the function name is not good
+    room.enPassant = checkEnPassant(
+      room.board,
+      currentSquare,
+      position,
+      room.turn
+    );
   }
 
-  // Select the opponent piece
-  const [pieceColor] = square;
-  return pieceColor === room.turn;
-}
+  /* Update state for 50-Move rule */
+  private static update50MoveState(
+    room: Room,
+    currentSquare: SquarePos,
+    position: SquarePos
+  ) {
+    if (shouldReset50Move(room.board, currentSquare, position)) {
+      room.fiftyMoveCount = 0;
+      return;
+    }
+    room.fiftyMoveCount++;
+  }
 
-// Get valid moves for the selected square
-function getValidMoves(room: Room, row: number, col: number) {
-  const { board, turn, enPassant, castlingRights } = room;
-  return getMoves(board, row, col, turn, enPassant, castlingRights[turn]);
+  /* Check if selected square is valid (first click) */
+  private static isSquareValidForSelection(
+    room: Room,
+    row: number,
+    col: number
+  ) {
+    const square = room.board[row][col];
+
+    // Select an empty square
+    if (!square) {
+      return false;
+    }
+
+    // Select the same piece twice
+    if (room.currentSquare === posString(row, col)) {
+      return false;
+    }
+
+    // Select the piece with correct color 
+    const [pieceColor] = square;
+    return pieceColor === room.turn;
+  }
+
+  /* Get valid moves for the selected square */
+  private static getValidMoves(room: Room, row: number, col: number) {
+    const { board, turn, enPassant, castlingRights } = room;
+    return getMoves(board, row, col, turn, enPassant, castlingRights[turn]);
+  }
 }
 
 export { selectSquareHandler, promotionHandler };
