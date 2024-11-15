855a054dbf7672f3ab90e8a0d48543956c43b57c
Son Do <dominhson580@gmail.com>
2024-11-15 11:16:41 +0700

    BE - use client event enum

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -14,24 +14,25 @@ import {
   resetRequestHandler,
 } from "./controllers/reset";
 import { GENERAL_CONFIG } from "./config";
+import { ClientEventName } from "./constants/event";
 
 const runSocketIO = (httpServer: HttpServer) => {
   const io = new Server(httpServer, {
     cors: { origin: GENERAL_CONFIG.CLIENT_URL },
   });
 
-  io.on("connection", (socket) => {
-    socket.on("createRoom", createRoomHandler(socket));
-    socket.on("joinRoom", joinRoomHandler(socket, io));
-    socket.on("leaveRoom", leaveRoomHandler(socket, io));
-    socket.on("disconnect", disconnectHandler(socket, io));
+  io.on(ClientEventName.Connection, (socket) => {
+    socket.on(ClientEventName.CreateRoom, createRoomHandler(socket));
+    socket.on(ClientEventName.JoinRoom, joinRoomHandler(socket, io));
+    socket.on(ClientEventName.LeaveRoom, leaveRoomHandler(socket, io));
+    socket.on(ClientEventName.Disconnect, disconnectHandler(socket, io));
 
-    socket.on("selectSquare", selectSquareHandler(socket, io));
-    socket.on("promote", promotionHandler(socket, io));
+    socket.on(ClientEventName.SelectSquare, selectSquareHandler(socket, io));
+    socket.on(ClientEventName.Promote, promotionHandler(socket, io));
 
-    socket.on("resetRequest", resetRequestHandler(socket, io));
-    socket.on("acceptReset", acceptResetHandler(socket, io));
-    socket.on("rejectReset", rejectResetHandler(socket, io));
+    socket.on(ClientEventName.ResetRequest, resetRequestHandler(socket, io));
+    socket.on(ClientEventName.AcceptReset, acceptResetHandler(socket, io));
+    socket.on(ClientEventName.RejectReset, rejectResetHandler(socket, io));
   });
 };
 
