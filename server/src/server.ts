4d136a0031292809606c0f8709ffda6db9a44caa
Son Do <dominhson580@gmail.com>
2026-09-11 17:21:23 +0700

    BE -  add health check endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,10 @@ import runSocketIO from "./socket";
 import { GENERAL_CONFIG } from "./config";
 
 const app = express();
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
+
 const server = createServer(app);
 runSocketIO(server);
 
